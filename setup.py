f19ae815760e8ba85ff03b3a850c0636a8b367f7
ZhuangXiang <zhuangxiang@zju.edu.cn>
2022-09-07 15:39:23 +0800

    add setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+import os
+
+setup(
+    name='bulk2space',
+    version='1.0.0',
+    description='De novo analysis of bulk RNA-seq data at spatially resolved single-cell resolution.',
+    author='',
+    author_email='',
+    url='https://github.com/ZJUFanLab/bulk2space',
+    packages=['bulk2space'],
+    install_requires=[],
+    license='GNU General Public License v3.0',
+)
